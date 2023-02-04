4c4107017eca16457d000549e5b8d75836d8b308
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-04 12:08:36 +0400

    Update FirstPage.jsx

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -4,16 +4,23 @@ import BodyLogo from "../assets/logo2.png";
 
 const FirstPage = () => {
     return (
-        <div className="bg ">
-            <img
-                src={HeaderLogo}
-                alt="redberry-logo"
-                className="py-[25px] pl-[90px]"
-            />
-            <hr className="w-[90%] border-1 border-black items-center mx-auto pt-6" />
-
+        <div className="bg flex flex-col">
             <div>
-                <a href="#" className="bg-black px-[126px] py-[18px] rounded-lg text-white">რეზიუმეს დამატება</a>
+                <img
+                    src={HeaderLogo}
+                    alt="redberry-logo"
+                    className="py-[25px] pl-[90px]"
+                />
+                <hr className="w-[90%] border-1 border-black items-center mx-auto pt-6" />
+            </div>
+
+            <div className="flex justify-center mx-auto items-center">
+                <button
+                    href="#"
+                    className="w-[464px] h-[60px] flex justify-center items-center mx-auto bg-black rounded-lg text-white"
+                >
+                    რეზიუმეს დამატება
+                </button>
             </div>
         </div>
     );
