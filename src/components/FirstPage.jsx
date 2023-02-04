700c0825250510bef34db2034698d1583ed0baf0
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-04 12:00:42 +0400

    Update FirstPage.jsx

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import HeaderLogo from "../assets/logo.png";
+import BodyLogo from "../assets/logo2.png";
 
 const FirstPage = () => {
     return (
-        <div>FirstPage</div>
-    )
-}
+        <div className="bg ">
+            <img
+                src={HeaderLogo}
+                alt="redberry-logo"
+                className="py-[25px] pl-[90px]"
+            />
+            <hr className="w-[90%] border-1 border-black items-center mx-auto pt-6" />
 
-export default FirstPage
+            <div>
+                <a href="#" className="bg-black px-[126px] py-[18px] rounded-lg text-white">რეზიუმეს დამატება</a>
+            </div>
+        </div>
+    );
+};
+
+export default FirstPage;
