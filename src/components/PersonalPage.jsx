7f9c672fd51405024581750ec1726963965a3ff2
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-05 14:08:48 +0400

    Update PersonalPage.jsx

--- src/components/PersonalPage.jsx
+++ src/components/PersonalPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import CancelArror from "../assets/cancel-arrow.png";
 
 const PersonalPage = () => {
     return (
-        <div>
-
+        <div className="h-screen w-1/2 pt-11 flex">
+            <div className="pl-12">
+                <Link to="/">
+                    <img src={CancelArror} alt="Cancel-arrow-icon" />
+                </Link>
+            </div>
+            <div className="w-full">
+                <div className="flex justify-between w-full pl-14 text-center">
+                    <h1 className="text-[#1A1A1A] font-bold text-2xl">პირადი ინფო</h1>
+                    <h2 className="text-[#1A1A1A] text-xl">1/3</h2>
+                </div>
+                <hr className="border-[#1A1A1A] mt-3 ml-14" />
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default PersonalPage
+export default PersonalPage;
