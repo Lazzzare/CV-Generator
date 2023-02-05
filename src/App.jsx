982a35985d0d2f206c1fc0a3ee6b27917b024610
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-05 12:07:18 +0400

    add/import pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import FirstPage from "./components/FirstPage"
+import PersonalPage from "./components/PersonalPage"
+import ExperiencePage from './components/ExperiencePage';
+import EducationPage from './components/EducationPage';
+import FinalResumePage from './components/FinalResumePage';
 import { Routes, Route } from "react-router-dom"
+
 
 function App() {
 
@@ -9,6 +14,10 @@ function App() {
       {/* <FirstPage /> */}
       <Routes>
         <Route path="/" element={<FirstPage />} />
+        <Route path="/personal" element={<PersonalPage />} />
+        <Route path="/experience" element={<ExperiencePage />} />
+        <Route path="/education" element={<EducationPage />} />
+        <Route path="/resume" element={<FinalResumePage />} />
       </Routes>
     </div>
   )
---
+++ src/components/FinalResumePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FinalResumePage = () => {
+    return (
+        <div>FinalResumePage</div>
+    )
+}
+
+export default FinalResumePage
