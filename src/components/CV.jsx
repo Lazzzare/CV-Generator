579f0299ded427ce360732c0cc87cdb291cb5c09
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-06 23:11:38 +0400

    Update CV.jsx

--- src/components/CV.jsx
+++ src/components/CV.jsx
@@ -4,7 +4,7 @@ import PhoneIcon from "../assets/Vector2.png";
 import ProfileImg from "../assets/profile-img.png";
 import { MdAlternateEmail } from 'react-icons/md'
 
-const CV = ({ name, lastName, mail, phone, about }) => {
+const CV = ({ name, lastName, mail, phone, about, image }) => {
     return (
         <div className="w-1/2 flex pl-[262px]">
             <div className="flex flex-col mt-16">
@@ -14,23 +14,23 @@ const CV = ({ name, lastName, mail, phone, about }) => {
                 </div>
                 <div className="">
                     <h2 className="flex items-center mt-5 text-[#1A1A1A] text-lg mb-3">
-                        <img src={GmailIcon} alt="gmail-icon" className="w-4 h-4 mr-3" />
+                        {/* <img src={GmailIcon} alt="gmail-icon" className="w-4 h-4 mr-3" /> */}
                         {mail}
                     </h2>
                     <h2 className="flex items-center text-[#1A1A1A] text-lg">
-                        <img src={PhoneIcon} alt="phone-icon" className="w-4 h-4 mr-3" />
+                        {/* <img src={PhoneIcon} alt="phone-icon" className="w-4 h-4 mr-3" /> */}
                         {phone}
                     </h2>
                 </div>
                 <h3 className="text-[#F93B1D] text-lg font-bold mt-8 mb-3">
-                    ჩემს შესახებ
+                    {/* ჩემს შესახებ */}
                 </h3>
                 <p className="max-w-[423px]">
                     {about}
                 </p>
             </div>
             <div className="mt-12 ml-6">
-                <img src={ProfileImg} alt="" className="w-[246px] h-[246px]" />
+                {/* <img src="" alt="" className="w-[246px] h-[246px]" /> */}
             </div>
         </div>
     );
